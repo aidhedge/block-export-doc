6b53684017fa3a2c69e53bf5090b4aa89cc55dfb
Ivo Lejon <ivo.lejon@gmail.com>
2018-12-06 12:40:08 +0100

    update

--- merger.py
+++ merger.py
@@ -2,6 +2,9 @@ import os
 from boto3.session import Session
 import boto3
 from mailmerge import MailMerge
+from logger import Logger
+
+LOG = Logger()
 
 AWS_ACCESSKEY_ID = os.environ.get('AWS_ACCESSKEY_ID')
 AWS_SECRET_KEY = os.environ.get('AWS_SECRET_KEY')
@@ -10,22 +13,26 @@ session = Session(aws_access_key_id=AWS_ACCESSKEY_ID,
                   aws_secret_access_key=AWS_SECRET_KEY)
 
 
-def merge(data, template, bucket_name, user):
+def merge(data, template, template_bucket_name, output_bucket_name, user):
     s3_res = session.resource('s3')
     s3 = boto3.client('s3')
-    bucket = s3_res.Bucket(bucket_name)
+    bucket = s3_res.Bucket(template_bucket_name)
+    # Download template from S3, save in /tmp
     bucket.download_file(template, '/tmp/temp.docx')
 
     document = MailMerge('/tmp/temp.docx')
+    # LOG.console(document.get_merge_fields())
+    document.merge_pages([data])
     document.write('/tmp/output.docx')
 
     key_file_name = "{0}/output.docx".format(user)
     data = open('/tmp/output.docx', 'rb')
-    s3_res.Bucket(bucket_name).put_object(Key=key_file_name, Body=data)
+    s3_res.Bucket(output_bucket_name).put_object(Key=key_file_name, Body=data)
+    
     url = s3.generate_presigned_url(
         ClientMethod='get_object',
         Params={
-            'Bucket': bucket_name,
+            'Bucket': output_bucket_name,
             'Key': key_file_name
         }
     )
